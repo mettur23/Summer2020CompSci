be6ed00f69884d071073c40c6aa5709e51708ec1
tejamettu <mettur23@greenhill.org>
2020-06-09 11:30:41 -0500

    Changed ints

--- src/com/company/moneycalc.java
+++ src/com/company/moneycalc.java
@@ -2,25 +2,40 @@ package com.company;
 
 public class moneycalc {
 
+  public static int item1 = 38;
+  public static int item2 = 40;
+  public static int item3 = 30;
+
     public static void main(String[] args) {
 
         // Calculate total owed, assuming 8% tax / 15% tip
 
-       int item1 = 38;
-       int item2 = 40;
-       int item3 = 30;
+        Subtotal();
+    Tax();
+      Tips();
+      Total();
+    }
 
+
+
+    public static void Subtotal () {
         System.out.println("Subtotal:");
 
         System.out.println(item1 + item2 + item3);
-
+    }
+    public static void Tax () {
         System.out.println("Tax:");
 
         System.out.println((item1 + item2 + item3) * .08);
+    }
 
-        System.out.println("Tip:");
+     public static void Tips ()  {
+            System.out.println("Tip:");
 
-        System.out.println((item1 + item2 + item3) * .15);
+            System.out.println((item1 + item2 + item3) * .15);
+        }
+
+        public static void Total (){
 
         System.out.println("Total:");
 
