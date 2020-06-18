4e5055bdd330bef215fddf9598f2e9f8ba6ed437
tejamettu <mettur23@greenhill.org>
2020-06-17 22:25:34 -0500

    Finished

---
+++ src/com/company/ChallengeOperators.java
@@ -0,0 +1,26 @@
+/*
+1. double MealCost = the user input, this will be the original cost of the meal that the user enters.
+In order to do this we need to do sc.nextDouble().
+    System.out.println ("Enter you Meal Cost: ")
+2. int TipRate = 0.15 * MealCost, this sets the tip rate at 15%.
+3. int TaxRate = 0.15 * MealCost, this sets the tax rate at 15%.
+4. int TotalCost = TipRate + TaxRate + MealCost.
+5. System.out.println ("Tip: " + TipRate)
+6. System.out.println ("Tax: " + TaxRate)
+7. System.out.println ("Total " + TotalCost)
+
+
+* */
+
+package com.company;
+
+public class ChallengeOperators {
+
+    public static void main(String[] args){
+
+
+
+
+
+    }
+}
