79f320b73059fcd9dd1fbbafdf76e6d5d055a5e1
tejamettu <mettur23@greenhill.org>
2020-06-19 22:01:50 -0500

    Finished

---
+++ src/com/company/ConditionalStatement.java
@@ -0,0 +1,108 @@
+/*
+Teja Mettu
+June 19, 2020
+1. Import java.util.Random
+2. Turn int n into a Random Number
+3. make an if statement that finds whether the int n is divisble by two without remainder
+4. if it is divisble without a remainder then println "Not weird"
+5. make an else statement if it has a remainder print weird
+6. If even and in the range 2 to 5 print Not Weird
+7. If even and in the range of 6 to 20 print Weird
+8. If even and not in between the 2 ranges check if greater than 20, if so then print Not Weird
+
+
+
+
+UPDATE (June 19th: After hours of work I can't manage to get the program to run properly,
+ the solve method isn't returning the if statements properly and
+ the Loop I setup in the main isn't getting stuck on solve and not hitting line 51)
+  */
+
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class ConditionalStatement {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in); //asks whether user wants to play
+        System.out.println("Do you want to play? Enter yes or no:");
+        String YesOrNo = sc.nextLine();
+        int loop = 0;
+
+        if (YesOrNo.equals("yes")) {
+            loop = 1;
+        }
+        if (YesOrNo.equals("no")) { // subtracts 1 to loop if they don't want to play
+            loop = 0;
+            System.out.println("Okay thanks for playing");
+        }
+
+        while (loop == 1) { // loop MUST be greater than or equal to 1 in order to run
+
+            solve(); // runs the method
+
+            loop = 0;
+
+            sc = new Scanner(System.in); //asks whether user wants to play again
+            System.out.println("Do you want to play again? Enter yes or no:");
+            YesOrNo = sc.nextLine();
+
+            if (YesOrNo.equals("yes")) { // if user wants to play it adds 1 to loop
+                loop=1;
+            }
+            if (YesOrNo.equals("no")) { // subtracts 1 to loop if they don't want to play
+
+                loop = 0;
+
+                System.out.println("Thanks for playing!");
+            }
+        }
+
+    }
+
+
+
+
+
+
+    public static void solve (){
+
+        Random rand = new Random();
+        int max = 137; //setting maximum value at 137
+        int n = rand.nextInt(max);
+        n += 1; //setting range of values between 1-137
+
+            while (n>=1 || n<=137){
+
+                if (n % 2 > 0){
+                    System.out.println("Weird");
+                }
+
+                if (n % 2 == 0) {
+
+                    }
+                    else if  (n>=2||n<=5){
+                        System.out.println("Not Weird");
+                    }
+                        else if (n>6||n<20){
+                             System.out.println("Weird");
+
+                             }
+                                 else if (n>20){
+
+                                     System.out.println("Not Weird");
+
+                                 }
+
+                                 n = 0;
+            }
+
+
+        }
+
+    }
---
+++ src/com/company/StringChallenge.java
@@ -0,0 +1,54 @@
+/*
+1. Initialize String S and Int N
+2. Create scanner input for String S
+3. identify String S indexes
+4. Set Integer T as the amount of Words in the String
+1<=T<=10
+2<=length of S<= 10000
+5. Main return method we used in the solve void
+6. Make a substring for the indexes
+
+Make variable for the substrings divded by 2. Out space after the return of the value of the index.
+
+
+*/
+
+
+package com.company;
+
+import java.util.Scanner;
+
+public class StringChallenge {
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in); //asks whether user wants to play
+        System.out.println("Please enter your words");
+       String word1 = sc.nextLine();
+       String word2 = sc.nextLine();
+       String word3 = sc.nextLine();
+       String word4 = sc.nextLine();
+       String word5 = sc.nextLine();
+       String word6 = sc.nextLine();
+       String word7 = sc.nextLine();
+       String word8 = sc.nextLine();
+       String word9 = sc.nextLine();
+       String word10 = sc.nextLine();
+
+       String AllWords = word1 + word2;
+
+    }
+    public static void solve (){
+
+
+
+
+    }
+
+
+}
+
+
+
