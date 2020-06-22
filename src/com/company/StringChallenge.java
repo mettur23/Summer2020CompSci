125f00aaf41333f2762f3848d4a9dd5a10ff198e
tejamettu <mettur23@greenhill.org>
2020-06-21 21:31:58 -0500

    Finished

--- src/com/company/StringChallenge.java
+++ src/com/company/StringChallenge.java
@@ -1,4 +1,6 @@
 /*
+Teja Mettu
+June/19/2020
 1. Initialize String S and Int N
 2. Create scanner input for String S
 3. identify String S indexes
@@ -7,10 +9,9 @@
 2<=length of S<= 10000
 5. Main return method we used in the solve void
 6. Make a substring for the indexes
+Make variable for the substrings divided by 2. Out space after the return of the value of the index.
 
-Make variable for the substrings divded by 2. Out space after the return of the value of the index.
-
-
+NOTE: I worked with Megan on this code, and I found a new strategy, so I didn't follow my initial pseudocode.
 */
 
 
@@ -22,33 +23,35 @@ public class StringChallenge {
 
     public static void main(String[] args) {
 
-
-
-        Scanner sc = new Scanner(System.in); //asks whether user wants to play
-        System.out.println("Please enter your words");
-       String word1 = sc.nextLine();
-       String word2 = sc.nextLine();
-       String word3 = sc.nextLine();
-       String word4 = sc.nextLine();
-       String word5 = sc.nextLine();
-       String word6 = sc.nextLine();
-       String word7 = sc.nextLine();
-       String word8 = sc.nextLine();
-       String word9 = sc.nextLine();
-       String word10 = sc.nextLine();
-
-       String AllWords = word1 + word2;
-
-    }
-    public static void solve (){
-
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String userWord = sc.nextLine();
+        String newWord = scramble(userWord);
+        System.out.println(newWord);
 
     }
 
+    public static String scramble (String userWord) {
+        String evenWord = " ";
+        String oddWord = " ";
 
-}
+        for (int even = 0; even < userWord.length(); even += 2) {
+            char temp = userWord.charAt(even);
+            evenWord = evenWord + temp;
+        }
+        for (int odd = 1; odd <userWord.length(); odd += 2) {
+            char temp = userWord.charAt(odd);
+            oddWord = oddWord + temp;
+        }
+String newWord = evenWord + " " + oddWord;
+        return newWord;
+
+    }
+
+    }
+
+
+
 
 
 
